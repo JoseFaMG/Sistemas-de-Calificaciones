48d46392d6d2a30abb1db0d8292b00536b549cf7
JoseFaMG <j.muxozcano.g656@edu.utc.mx>
2023-11-21 21:50:26 -0600

    1.6

--- crudalumnos.php
+++ crudalumnos.php
@@ -4,10 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/estilo.css">
     <title>Registro de Alumnos</title>
+    <link rel="shortcut icon" href="assets/Images/utc.png" />
 </head>
 <body>
-    <h1 class="txt-center p-3">Registro de Alumnos</h1>
+    <h1 class="text-center p-3">Registro de Alumnos</h1>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
