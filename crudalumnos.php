2a9d438f837c3515fe98d1f9e2e47ee544b7940c
JoseFaMG <j.muxozcano.g656@edu.utc.mx>
2023-11-21 22:58:41 -0600

    1.8

--- crudalumnos.php
+++ crudalumnos.php
@@ -9,7 +9,30 @@
     <link rel="shortcut icon" href="assets/Images/utc.png" />
 </head>
 <body>
+
     <h1 class="text-center p-3">Registro de Alumnos</h1>
+    <div class="container-fluid row">
+    <div class="container" id="container"><
+        <form class="col-4">
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+    </div>
+
+
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
