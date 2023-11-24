bbd7280f8dfec362d73ba0e387da015b0088e31f
JoseFaMG <j.muxozcano.g656@edu.utc.mx>
2023-11-23 19:23:59 -0600

    actualizar

---
+++ loginal.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/stiles.css">
+    <title>Login para docentes</title>
+</head>
+<body>
+    
+    <div class="container">
+
+        <div class="info">
+            <p class="txt-1">Bienbenido!</p>
+        </div>
+
+    </div>
+
+
+</body>
+</html>
